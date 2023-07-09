f6e35a41b31c8269e7de2f5409b6618d7cfd0251
Tyler Neely <t@jujit.su>
2023-07-09 18:40:16 +0200

    Bump version for release that includes dual-variant heap storage

--- README.md
+++ README.md
@@ -7,19 +7,20 @@ are being shared in database-like scenarios, where optimizing for space usage is
 important.
 
 `InlineArray` uses 8 bytes on the stack. It will inline arrays of up to 7 bytes. If the bytes
-are longer than that, it will store them in an optimized reference-count-backed structure,
-where the atomic reference count is 16 bytes. If the maximum counter is reached, the bytes
-are copied into a new `InlineArray` with a fresh reference count of 1. This is made with
-the assumption that most reference counts will be far lower than 2^16.
+are longer than that, it will store them in an optimized reference-count-backed structure of
+two different variants. For arrays up to length 255, the data is stored with an `AtomicU8`
+reference counter and `u8` length field, for only two bytes of overhead. For values larger
+than that, they are stored with an `AtomicU16` reference counter and a 48-bit length field.
+If the maximum counter is reached for either variant, the bytes are copied into a new
+`InlineArray` with a fresh reference count of 1. This is made with the assumption that most
+reference counts will be far lower than 2^16 and only rarely surpassing 255 in the small case.
 
-Both the inline and shared instances of `InlineArray` guarantee that the stored array is
+The inline and both types of shared instances of `InlineArray` guarantee that the stored array is
 always aligned to 8-byte boundaries, regardless of if it is inline on the stack or
 shared on the heap. This is advantageous for using in combination with certain
-zero-copy serialization techniques.
+zero-copy serialization techniques that require alignment guarantees.
 
-The 16-bit reference counter is stored packed with a 48-bit length field at the beginning
-of the shared array. Byte arrays that require more than 48 bits to store their length
-(256 terabytes) are not supported.
+Byte arrays that require more than 48 bits to store their length (256 terabytes) are not supported.
 
 `InlineArray::make_mut` can be used for getting a mutable reference to the bytes in this
 structure. If the shared reference counter is higher than  1, this acts like a `Cow` and
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 #![allow(unsafe_code)]
 
-//! [`InlineArray`] is an inlinable array of bytes that is intended for situations where many bytes
+//! [`InlineArray`] is a stack-inlinable array of bytes that is intended for situations where many bytes
 //! are being shared in database-like scenarios, where optimizing for space usage is extremely
 //! important.
 //!
-//! [`InlineArray`] uses 8 bytes on the stack. It will inline arrays of up to 7 bytes. If the bytes
-//! are longer than that, it will store them in an optimized reference-count-backed structure,
-//! where the atomic reference count is 16 bytes. If the maximum counter is reached, the bytes
-//! are copied into a new `InlineArray` with a fresh reference count of 1. This is made with
-//! the assumption that most reference counts will be far lower than 2^16.
+//! `InlineArray` uses 8 bytes on the stack. It will inline arrays of up to 7 bytes. If the bytes
+//! are longer than that, it will store them in an optimized reference-count-backed structure of
+//! two different variants. For arrays up to length 255, the data is stored with an `AtomicU8`
+//! reference counter and `u8` length field, for only two bytes of overhead. For values larger
+//! than that, they are stored with an `AtomicU16` reference counter and a 48-bit length field.
+//! If the maximum counter is reached for either variant, the bytes are copied into a new
+//! `InlineArray` with a fresh reference count of 1. This is made with the assumption that most
+//! reference counts will be far lower than 2^16 and only rarely surpassing 255 in the small case.
 //!
-//! Both the inline and shared instances of `InlineArray` guarantee that the stored array is
+//! The inline and both types of shared instances of `InlineArray` guarantee that the stored array is
 //! always aligned to 8-byte boundaries, regardless of if it is inline on the stack or
 //! shared on the heap. This is advantageous for using in combination with certain
-//! zero-copy serialization techniques.
+//! zero-copy serialization techniques that require alignment guarantees.
 //!
-//! The 16-bit reference counter is stored packed with a 48-bit length field at the beginning
-//! of the shared array. Byte arrays that require more than 48 bits to store their length
-//! (256 terabytes) are not supported.
+//! Byte arrays that require more than 48 bits to store their length (256 terabytes) are not supported.
 //!
-//! `InlineArray::make_mut` can be used for getting a mutable reference to the bytes in this
-//! structure. If the shared reference counter is higher than  1, this acts like a `Cow` and
-//! will make self into a private copy that is safe for modification.
+//! [`InlineArray::make_mut`] (inspired by [`std::sync::Arc::make_mut`]) can be used for getting a mutable
+//! reference to the bytes in this structure. If the shared reference counter is higher than  1, this acts
+//! like a [`std::borrow::Cow`] and will make self into a private copy that is safe for modification.
 
 use std::{
     alloc::{alloc, dealloc, Layout},
